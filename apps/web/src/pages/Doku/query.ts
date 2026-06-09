ba4e8c6760c371492a85efa5afcdac807acddb6a
Matteo Lutz <matteo.lutz06@outlook.de>
2026-06-09 13:25:59 +0200

    fix(perf doku rendering): memoize selection fn

--- apps/web/src/pages/Doku/query.ts
+++ apps/web/src/pages/Doku/query.ts
@@ -5,7 +5,30 @@ import {
   prependTableOfContents,
   type DokuOrderPage
 } from './order';
-import type { AcademyWithCourses } from '@repo/db/types';
+import type { AcademyWithCourses, Document } from '@repo/db/types';
+import { useCallback } from 'react';
+
+const useSelectFunction = (
+  academyId: number,
+  academy: AcademyWithCourses | null
+) =>
+  useCallback(
+    (data: Document[]): DokuOrderPage[] => {
+      const order = makeDokuOrder(data, {
+        breakToEvenPageOnNewCategory: false
+      });
+      console.log('order', order);
+
+      const pages = prependTableOfContents(order, academy!, {
+        tocStartingPageIndex: 2
+      });
+
+      console.log('pages', pages);
+
+      return [{ type: 'cover', academyId }, { type: 'blank' }, ...pages];
+    },
+    [academyId, academy]
+  );
 
 export const useAcademyDocsQuery = (
   academyId: number,
@@ -16,20 +39,7 @@ export const useAcademyDocsQuery = (
       { academyId },
       {
         enabled: academy !== null,
-        select: (data): DokuOrderPage[] => {
-          const order = makeDokuOrder(data, {
-            breakToEvenPageOnNewCategory: false
-          });
-          console.log('order', order);
-
-          const pages = prependTableOfContents(order, academy!, {
-            tocStartingPageIndex: 2
-          });
-
-          console.log('pages', pages);
-
-          return [{ type: 'cover', academyId }, { type: 'blank' }, ...pages];
-        }
+        select: useSelectFunction(academyId, academy)
       }
     )
   );
