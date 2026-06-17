084b400edbb432463c5772d371ac55782ab5f605
Matteo Lutz <matteo.lutz06@outlook.de>
2026-06-17 12:26:21 +0200

    fix(academy selection): automatically select first academy when none is selected

--- apps/web/src/components/fm/academySelector.tsx
+++ apps/web/src/components/fm/academySelector.tsx
@@ -8,11 +8,30 @@ import {
   SelectValue
 } from '../ui/select';
 import { useQuery } from '@tanstack/react-query';
+import { useEffect, useEffectEvent } from 'react';
 
 const AcademySelector = () => {
   const [loggedInState, setLoggedInState] = useLoggedInState();
 
   const academiesQuery = useQuery(trpc.academy.getSelectable.queryOptions());
+
+  const selectAcademy = useEffectEvent((academyId: number) => {
+    setLoggedInState((current) => ({
+      ...current,
+      selectedAcademy: academyId
+    }));
+  });
+  useEffect(() => {
+    if (
+      typeof academiesQuery.data === 'undefined' ||
+      academiesQuery.data.length === 0
+    )
+      return;
+
+    if (loggedInState.selectedAcademy !== null) return;
+
+    selectAcademy(academiesQuery.data[0].id);
+  }, [academiesQuery.data, loggedInState]);
 
   return (
     <Select
