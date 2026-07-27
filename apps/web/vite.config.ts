2155db430909fe36a10f8c547d83e1e0d53a6f5b
Matteo Lutz <matteo.lutz06@outlook.de>
2026-07-27 18:20:53 +0200

    feat(web): runtime env vars

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,57 +1,23 @@
-import {
-  defineConfig,
-  loadEnv,
-  type HtmlTagDescriptor,
-  type Plugin
-} from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 import path from 'path';
 import { VitePWA } from 'vite-plugin-pwa';
-
-/// Helper function to get a required environment variable.
-const requiredEnv = (env: Record<string, string>, name: string): string => {
-  const value = env[name];
-
-  if (typeof value === 'undefined' || value === '')
-    throw new Error(`Missing required environment variable: ${name}`);
-
-  return value;
-};
-
-/// Vite plugin to inject theme variables into the HTML head.
-const injectTheme = (vars: Record<string, string>): Plugin => ({
-  name: 'inject-theme',
-  enforce: 'post',
-  transformIndexHtml: () => {
-    const tagDescriptor: HtmlTagDescriptor = {
-      injectTo: 'head',
-      tag: 'style',
-      attrs: { type: 'text/css' },
-      children: `@layer theme { :root {${Object.entries(vars)
-        .map(([key, value]) => `--${key}: ${value}`)
-        .join(';')}}}`
-    };
-
-    return [tagDescriptor];
-  }
-});
+import runtimeEnv from 'vite-plugin-runtime-env';
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd());
-
+export default defineConfig(() => {
   const pwa = VitePWA({
     registerType: 'autoUpdate',
     devOptions: {
       enabled: true
     },
     manifest: {
-      name: requiredEnv(env, 'VITE_APP_NAME'),
-      short_name: requiredEnv(env, 'VITE_APP_SHORT_NAME'),
+      name: 'DSA Doku',
+      short_name: 'DSA Doku',
       start_url: '/',
-      background_color: requiredEnv(env, 'VITE_APP_BACKGROUND_COLOR'),
-      theme_color: requiredEnv(env, 'VITE_APP_THEME_COLOR'),
+      background_color: '#ffffff',
+      theme_color: '#00858c',
       icons: [
         {
           src: `./src/assets/icon-192.png`,
@@ -68,20 +34,7 @@ export default defineConfig(({ mode }) => {
   });
 
   return {
-    plugins: [
-      react(),
-      tailwindcss(),
-      pwa,
-      injectTheme({
-        'pwa-background-color': requiredEnv(env, 'VITE_APP_BACKGROUND_COLOR'),
-        'pwa-theme-color': requiredEnv(env, 'VITE_APP_THEME_COLOR'),
-        'pwa-theme-color-foreground': requiredEnv(
-          env,
-          'VITE_APP_THEME_COLOR_FOREGROUND'
-        ),
-        'pwa-theme-color-soft': requiredEnv(env, 'VITE_APP_THEME_COLOR_SOFT')
-      })
-    ],
+    plugins: [react(), tailwindcss(), pwa, runtimeEnv()],
     resolve: {
       alias: {
         '@': path.resolve(__dirname, './src')
